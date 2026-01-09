93b2389434e372c4b81a1378cea9a0e410080738
Niklas Weber <nweber@student.42Heilbronn.de>
2026-01-09 14:14:30 +0100

    add: implement ConfigException class for handling configuration errors

---
+++ src/configException.cpp
@@ -0,0 +1,20 @@
+#include "parsing.hpp"
+
+Config::ConfigException::ConfigException() : message("Configuration Exception") {}
+
+Config::ConfigException::ConfigException(const std::string& msg) : message("Configuration Exception: " + msg) {}
+
+Config::ConfigException::ConfigException(const ConfigException& other) : message(other.message) {}
+
+Config::ConfigException& Config::ConfigException::operator=(const ConfigException& other) {
+    if (this != &other) {
+        message = other.message;
+    }
+    return *this;
+}
+
+Config::ConfigException::~ConfigException() {}
+
+const char* Config::ConfigException::what() {
+    return message.c_str();
+}
