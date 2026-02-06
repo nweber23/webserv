370a5ee5730e8717119d1eaf30db4d6ee01baaff
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-02-06 15:43:57 +0100

    Update Base Middleware

--- inc/AMiddleware.hpp
+++ inc/AMiddleware.hpp
@@ -2,6 +2,8 @@
 
 #include "IMiddleware.hpp"
 
+
+// TODO: Is this constructors okay?? May be not. Check in the next versions
 class AMiddleware : public IMiddleware
 {
 protected:
@@ -10,5 +12,10 @@ protected:
 	bool callNext(HttpRequest& request, HttpResponse& response);
 
 public:
+	AMiddleware() = default;
+	AMiddleware(const AMiddleware& other) = delete;
+	AMiddleware& operator=(const AMiddleware& other) = delete;
+	~AMiddleware() override = default;
+
 	void setNext(std::unique_ptr<IMiddleware> next) override;
 };
--- interfaces/IMiddleware.hpp
+++ interfaces/IMiddleware.hpp
@@ -10,16 +10,13 @@
 class IMiddleware
 {
 private:
+	IMiddleware() = default;
+	
 	IMiddleware(const IMiddleware& ather) = delete;
 	IMiddleware& operator=(const IMiddleware& ather) = delete;
-
 public:
-	IMiddleware() = default;
 	virtual ~IMiddleware() = default;
-
-	IMiddleware(IMiddleware&& ather) = default;
-    IMiddleware& operator=(IMiddleware&& ather) = default;
-
+	
 	virtual void setNext(std::unique_ptr<IMiddleware> next) =0;
 	virtual bool handle(HttpRequest& request, HttpResponse& response) =0; 
 };
